c9ebd3c961ea19d519434d145dfb0f834b43c131
Alan Chang <tcode2k16@gmail.com>
2018-07-16 00:20:49 +0800

    wip

--- src/index.js
+++ src/index.js
@@ -1,16 +1,41 @@
+const fs = require('fs');
 const puppeteer = require('puppeteer');
 
 const timeout = ms => new Promise(res => setTimeout(res, ms));
 
 (async () => {
   // const gameID = 12345
+  const localStoragePath = './gameData.json';
+  const groupCount = 10;
   const browser = await puppeteer.launch({
     headless: false,
+    // executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google\ Chrome',
   });
   const page = await browser.newPage();
   await page.setViewport({ width: 1440, height: 780});
 
-  await page.goto('http://generals.io/');
+  // await page.goto('http://generals.io/');
+  await page.goto('http://generals.io/games/12345');
+
+  if (fs.existsSync(localStoragePath) && fs.lstatSync(localStoragePath).isFile()) {
+    let storage = fs.readFileSync(localStoragePath, 'utf8');
+    console.log(storage);
+    try {
+      await page.evaluate((storage) => {
+        for (let key in storage) {
+          localStorage.setItem(key, storage[key]);
+        }
+      }, JSON.parse(storage));
+  
+      await page.reload();
+    } catch (e) {
+      console.error(`Error restoring localStorage: ${e.message}`)
+    }
+  }
+
+  await page.addScriptTag({
+    path: '../lib/pathfinding-browser.min.js'
+  });
   await page.exposeFunction('click', async (x, y) => {
     await page.click(`#map > tbody > tr:nth-child(${y+1}) > td:nth-child(${x+1})`, {
       delay: 10
@@ -49,7 +74,194 @@ const timeout = ms => new Promise(res => setTimeout(res, ms));
             )
         );
     }
+    function getGroupActions(cells) {
+      let groupActions = [];
+      for (const row of cells) {
+        for (const cell of row) {
+          if (cell.kind.indexOf(color) >= 0) {
+            let actions = _getGroupAction(cell, cells);
+            groupActions = [
+              ...actions,
+              ...groupActions
+            ];
+          }
+        }
+      }
+      return groupActions;
+    }
     
+    function _getGroupAction(cell, cells) {
+      let x = cell.x;
+      let y = cell.y;
+      let xl = x - 1;
+      let xr = x + 1;
+      let yu = y - 1;
+      let yd = y + 1;
+      const needItems = [color];
+      // let maxActions = cell.value === NaN ? 0 : cell.value - 1;
+      let maxActions = isNaN(cell.value) || cell.value < groupLimit ? 0 : 1;
+    
+      let actions = [];
+
+      if (maxActions > 0 && inRange(xl, y) && cells[y][xl].kind.some(r => needItems.indexOf(r) >= 0) && !isNaN(cells[y][xl].value) && cells[y][xl].value > cell.value) {
+        maxActions--;
+        actions.push({
+          from: {
+            x,
+            y,
+          },
+          to: {
+            x: xl,
+            y
+          }
+        });
+        cells[y][x].value = 1;
+        cells[y][xl].value += cell.value-1;
+      }
+    
+      if (maxActions > 0 && inRange(xr, y) && cells[y][xr].kind.some(r => needItems.indexOf(r) >= 0) && !isNaN(cells[y][xr].value) && cells[y][xr].value > cell.value) {
+        maxActions--;
+        actions.push({
+          from: {
+            x,
+            y,
+          },
+          to: {
+            x: xr,
+            y
+          }
+        });
+        cells[y][x].value = 1;
+        cells[y][xr].value += cell.value-1;
+      }
+    
+      if (maxActions > 0 && inRange(x, yu) && cells[yu][x].kind.some(r => needItems.indexOf(r) >= 0) && !isNaN(cells[yu][x].value) && cells[yu][x].value > cell.value) {
+        maxActions--;
+        actions.push({
+          from: {
+            x,
+            y,
+          },
+          to: {
+            x,
+            y: yu
+          }
+        });
+        cells[y][x].value = 1;
+        cells[yu][x].value += cell.value-1;
+      }
+    
+      if (maxActions > 0 && inRange(x, yd) && cells[yd][x].kind.some(r => needItems.indexOf(r) >= 0) && !isNaN(cells[yd][x].value) && cells[yd][x].value > cell.value) {
+        maxActions--;
+        actions.push({
+          from: {
+            x,
+            y,
+          },
+          to: {
+            x,
+            y: yd
+          }
+        });
+        cells[y][x].value = 1;
+        cells[yd][x].value += cell.value-1;
+      }
+    
+      return actions;
+    }
+
+    function getAggressiveActions(cells) {
+      let aggressiveActions = [];
+      for (const row of cells) {
+        for (const cell of row) {
+          if (cell.kind.indexOf(color) >= 0) {
+            let actions = _getAggressiveAction(cell, cells);
+            aggressiveActions = [
+              ...actions,
+              ...aggressiveActions
+            ];
+          }
+        }
+      }
+      return aggressiveActions;
+    }
+    
+    function _getAggressiveAction(cell, cells) {
+      let x = cell.x;
+      let y = cell.y;
+      let xl = x - 1;
+      let xr = x + 1;
+      let yu = y - 1;
+      let yd = y + 1;
+      const avoidItems = [color, 'mountain', 'going to fill', ''];
+      // let maxActions = cell.value === NaN ? 0 : cell.value - 1;
+      let maxActions = isNaN(cell.value) || cell.value === 1 ? 0 : 1;
+    
+      let actions = [];
+
+      if (maxActions > 0 && inRange(xl, y) && cells[y][xl].kind.every(r => avoidItems.indexOf(r) < 0) && (isNaN(cells[y][xl].value) || cells[y][xl].value+1 < cell.value)) {
+        maxActions--;
+        actions.push({
+          from: {
+            x,
+            y,
+          },
+          to: {
+            x: xl,
+            y
+          }
+        });
+        cells[y][xl].kind[0] = 'going to fill';
+      }
+    
+      if (maxActions > 0 && inRange(xr, y) && cells[y][xr].kind.every(r => avoidItems.indexOf(r) < 0) && (isNaN(cells[y][xr].value) || cells[y][xr].value+1 < cell.value)) {
+        maxActions--;
+        actions.push({
+          from: {
+            x,
+            y,
+          },
+          to: {
+            x: xr,
+            y
+          }
+        });
+        cells[y][xr].kind[0] = 'going to fill';
+      }
+    
+      if (maxActions > 0 && inRange(x, yu) && cells[yu][x].kind.every(r => avoidItems.indexOf(r) < 0) && (isNaN(cells[yu][x].value) || cells[yu][x].value+1 < cell.value)) {
+        maxActions--;
+        actions.push({
+          from: {
+            x,
+            y,
+          },
+          to: {
+            x,
+            y: yu
+          }
+        });
+        cells[yu][x].kind[0] = 'going to fill';
+      }
+    
+      if (maxActions > 0 && inRange(x, yd) && cells[yd][x].kind.every(r => avoidItems.indexOf(r) < 0) && (isNaN(cells[yd][x].value) || cells[yd][x].value+1 < cell.value)) {
+        maxActions--;
+        actions.push({
+          from: {
+            x,
+            y,
+          },
+          to: {
+            x,
+            y: yd
+          }
+        });
+        cells[yd][x].kind[0] = 'going to fill';
+      }
+    
+      return actions;
+    }
+
     function getExpandActions(cells) {
       let expandActions = [];
       for (const row of cells) {
@@ -75,7 +287,7 @@ const timeout = ms => new Promise(res => setTimeout(res, ms));
       let yd = y + 1;
     
       // let maxActions = cell.value === NaN ? 0 : cell.value - 1;
-      let maxActions = cell.value === NaN || cell.value === 1 ? 0 : 1;
+      let maxActions = isNaN(cell.value) || cell.value === 1 ? 0 : 1;
     
       let actions = [];
     
@@ -171,11 +383,37 @@ const timeout = ms => new Promise(res => setTimeout(res, ms));
     
         handled = true;
         console.log('f pressed');
-        let cells = getCells();
         let expandActions = getExpandActions(getCells());
         for (const move of expandActions) {
-          console.log('called');
-          console.log(cells[move.from.y][move.from.x].el);
+          await window.clearSelect();
+          await window.click(move.from.x, move.from.y);
+          await window.click(move.to.x, move.to.y);
+        }
+      } else if (e.keyCode === 71) {  // g -> group
+        if (!isInit) {
+          alert('init first');
+          return;
+        }
+    
+        handled = true;
+        console.log('g pressed');
+        let groupActions = getGroupActions(getCells());
+        for (const move of groupActions) {
+          await window.clearSelect();
+          await window.click(move.from.x, move.from.y);
+          await window.click(move.to.x, move.to.y);
+        }
+      } else if (e.keyCode === 69) {  // e -> aggressive fill
+        if (!isInit) {
+          alert('init first');
+          return;
+        }
+    
+        handled = true;
+        console.log('e pressed');
+        let aggressiveActions = getAggressiveActions(getCells());
+        console.log(aggressiveActions);
+        for (const move of aggressiveActions) {
           await window.clearSelect();
           await window.click(move.from.x, move.from.y);
           await window.click(move.to.x, move.to.y);
@@ -193,5 +431,23 @@ const timeout = ms => new Promise(res => setTimeout(res, ms));
         e.preventDefault();
       }
     });
+  });
+
+
+  process.on('SIGINT', async () => {
+    console.log("Caught interrupt signal");
+    let storage = await page.evaluate(() => {
+      let value, storage = {};
+      for (let key in localStorage) {
+        if (value = localStorage.getItem(key))
+          storage[key] = value;
+      }
+
+      return storage;
+    });
+    fs.writeFileSync(localStoragePath, JSON.stringify(storage))
+      
+    await browser.close()
+    process.exit();
   });
 })();
